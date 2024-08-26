f2f57b9a3f25a326a925c5f71efa67d70e98bb98
Tirth S Pandya <teamfznltd@gmail.com>
2024-08-26 17:51:59 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // node index.js in terminal
-console.log("Hello");
+// console.log("Hello");
+let a = 1; // variable defining
+a = 2; // now changing its value to 2
+console.log(a); // printing the content on the console
+console.log(2);
+console.log(43);
+console.log("shit got real today")
+const b = 43; // the value cannot be changed
+console.log(b);
+
+// string bool and number
+let name = "Harkirat";
+let married = false;
+let age = 19;
+console.log(name, married, age);
+console.log("his name is" + name + "age is" + age);
+
+
+//1
+
