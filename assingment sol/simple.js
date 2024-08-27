955375c1c37635e49ce9b8777204d00d8c88024b
Tirth S Pandya <teamfznltd@gmail.com>
2024-08-27 15:26:16 +0530

    commit

---
+++ assingment sol/simple.js
@@ -0,0 +1,23 @@
+// 1. Write the program to greet a person given their first and last name
+let firstname = "Tirth";
+let lastname = "Pandya";
+console.log("Hello!!"  +" "+firstname  +" "+  lastname);
+
+// 2. Write a program that greets a person based on their gender. (If else)
+
+let name = "tirth";
+let gender = "female";
+if(gender == "male"){
+    console.log("hello uncle!");
+}
+else if(gender == "female"){
+    console.log("hello aunty");
+}
+else{
+    console.log("there exist only 2 genders");
+}
+
+// 3. Write a program that counts from 0 - 1000 and prints (for loop)
+for(let i=0; i<=1000; i++){
+    console.log(i);
+}
