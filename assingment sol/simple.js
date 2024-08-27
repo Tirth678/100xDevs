5db821de79393e0fb4751c6e71454af517ae39e6
Tirth S Pandya <teamfznltd@gmail.com>
2024-08-27 16:05:08 +0530

    commit

--- assingment sol/simple.js
+++ assingment sol/simple.js
@@ -21,3 +21,35 @@ else{
 for(let i=0; i<=1000; i++){
     console.log(i);
 }
+
+
+
+// 1. Write a program prints all the even numbers in an array
+
+// const num = [1,2,3,4,5,6,7,8,9,10];
+// for(let i=0; i<num.length; i++){
+//     if(num[i]%2 == 0){
+//         console.log(num[i]);
+//     }
+// }
+
+// 2. Write a program to print the biggest number in an arrya
+function largestelement(number){
+    let max = number[0];
+    for(let i=0; i<number.length; i++){
+        if(number[i]>max){
+            max = number[i];
+        }
+    }
+    return max;
+}
+
+// 3. Write a program that prints all the male people’s first name given a complex object
+
+const person = ["kirat", "soham", "priya"];
+const genderarr = ["male", "male", "female"];
+for(let i=0; i<person.length; i++){
+    if(person[i] == "male"){
+        console.log(genderarr[i]);
+    }
+}
