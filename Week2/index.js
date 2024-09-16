a365313d5f7607c6e68d6027732b63307d068185
Tirth S Pandya <teamfznltd@gmail.com>
2024-09-16 16:08:44 +0530

    commit

--- Week2/index.js
+++ Week2/index.js
@@ -3,3 +3,15 @@
 // ECMAScript, JavaScript, Node.js, bun
 // specification on which js is based
 
+// writing express code here
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
