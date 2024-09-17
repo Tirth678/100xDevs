8d984ad8bdf4b73a65dd79f0805c9456a1380d10
Tirth S Pandya <teamfznltd@gmail.com>
2024-09-17 10:41:57 +0530

    commit

--- Week2/bash.js
+++ Week2/bash.js
@@ -1,9 +1,26 @@
 // terminal and bash shortcuts and functions
+// terminal is nothing but another interface to do things in your machine
 1. pwd (print working directory)
-// base directory 
-2. cd
-3. ls
-4. mkdir
-5. touch
-6. cat
-7. vi
+// base directory of your system (path to my currrent folder)
+2. cd (change directory)
+// cd to go in a folder,
+// cd.. to come out of a folder(one folder before the current folder)
+// cd../.. will take out 2 folders behind
+3. ls (listing all the files in the current folder)
+// gives out a list of every file in folder
+// desktop/ ls => every document
+4. mkdir (make directory)
+// make new folders
+5. touch (lets you create an empty file)
+6. cat (prints the content of the files)
+// cat index.txt
+7. vi (vim)
+// popular editor used for coding btw
+8. mv (move your file)
+// mv a2.txt new-folder/
+9. cp (copy)
+// copy files
+// 
+10. nvm
+11. npm
+12. node
