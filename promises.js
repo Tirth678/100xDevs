f68e77333b7bd2917058070ec261dd0b6168b699
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-06 17:29:20 +0530

    commit

--- promises.js
+++ promises.js
@@ -14,4 +14,23 @@ ans.then(function(){
 });
 
 //how to call a promisified function
-
+// promise chaining
+function promisifiedTimeout(duration){
+    const p = new Promise(function(resolve){
+        setTimeout(resolve, duration);
+    })
+    return p;
+}
+// aynsc await
+promisifiedFunction(1000).then(function(){
+    console.log("first one is done");
+    promisifiedFunction(2000).then(function(){
+        console.log("second one is done");
+    })
+})
+promisifiedFunction(1000).then(function(){
+    console.log("first one is done");
+    return promisifiedFunction(2000).then(function(){
+        console.log("second one is done");
+    })
+});
