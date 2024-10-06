ee27de9ae418052f0bc9aaded3ec4ccb880b101e
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-06 16:09:53 +0530

    commit

---
+++ promises.js
@@ -0,0 +1,14 @@
+// defining a promise
+function promisifiedFunction(duration){
+    const p = new Promise(function(resolve){
+        setTimeout(function(){
+            resolve();
+        }, duration);
+    });
+    return p;
+}
+// calling a promise
+const ans = promisifiedFunction(1000);
+ans.then(function(){
+    console.log("done");
+});
