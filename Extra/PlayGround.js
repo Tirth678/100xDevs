e79e00497137ff4f6433d53395c9b3b033f90a40
Tirth S Pandya <teamfznltd@gmail.com>
2025-01-31 11:42:45 +0530

    .

--- Extra/PlayGround.js
+++ Extra/PlayGround.js
@@ -1,6 +1,21 @@
-// convert a given string to uppercase
-const toUpper = (str) => {
-    console.log("Original string: ", str);
-    console.log("Upper-case: ", str.toUpperCase());
+function square(n){
+    return n*n;
 }
-toUpper("Hello");
+function cube(n){
+    return n*n*n;
+}
+function quad(n){
+    n*n*n*n;
+}
+function doSomething(a,b, fn){
+    let op1 = fn(a);
+    let op2 = fn(b);
+    return op1 + op2;
+}
+function doNothing(z,y, fx){
+    return fx(z) + fx(y);
+}
+let ans = doSomething(1,2, square);
+console.log(ans)
+let ans2 = doNothing(1,2, square);
+console.log(ans2)
