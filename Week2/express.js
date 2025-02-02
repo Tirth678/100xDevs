e0b185ac9c179cd7308e3b131ae2f9c88e260f1e
Tirth S Pandya <teamfznltd@gmail.com>
2025-02-02 11:10:47 +0530

    .

--- Week2/express.js
+++ Week2/express.js
@@ -2,6 +2,18 @@
 // HTTP: Hyper Text Transfer Protocol
 // ECMAScript, JavaScript, Node.js, bun
 // scripting language specification on which js is based
+// whoever wants to make a compiler for JS, keep this things in mind said by ECMA
+
+
+//JS:
+// is a scripting lang that conforms to ECMAScript specification, most widely used and implementation of ECMAScript
+// JS includes parts which are not part of ECMA standards(setTimeout, DOM, fs.readFile)
+
+// HTTP: Hyper text transfer protocol
+// protocol defines for machines to communicate
+// how froentend communicates with backend
+
+// DNS Resolution: Domain Name Serivice Resolution
 
 // writing express code here
 const express = require('express');
