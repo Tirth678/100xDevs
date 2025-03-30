5c07f686981574dbb4ccb953d121682b8c6d8668
Tirth S Pandya <teamfznltd@gmail.com>
2025-03-30 11:42:40 +0530

    deleting old files

--- assingment sol/myarrow.js
+++
@@ -1,8 +0,0 @@
-// create a map fn that takes an array and a transform fn as input and returns the transformed array as output
-const newArr = []
-function myArrow(arr, fn){
-    for(let i=0; i<arr.length; i++){
-        newArr.push(fn(arr[i]))
-    }
-    return newArr;
-}
