126604b4406e3097a9b5959fb2f7b75649d750d7
Tirth S Pandya <teamfznltd@gmail.com>
2025-04-04 21:55:52 +0530

    started week 1

---
+++ Foundation/Javascript/index.js
@@ -0,0 +1,24 @@
+console.log("Hello world");
+// js is an interpreated language means it checks line by line code, and does not require whole to compile first and then covert it into 0's and 1's and then execute the program.
+// it is able to run program partially
+// some basic programs done in class
+//1
+let firstName = "kirat";
+let lastName = "singh"
+console.log(`HELLO ${firstName} ${lastName} `);
+
+//2
+let gender = "male";
+if(gender == "male"){
+    console.log("Hi sir");
+}
+else{
+    console.log("Hi ma'am")
+}
+
+//3 
+let n = 1000;
+for(let i=0; i<=n; i++){
+    console.log(i);
+    console.log("\n");
+}
